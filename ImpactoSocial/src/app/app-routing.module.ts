4e60d68086f816757c5937494ec43ba62cca86a3
Luccas Yuri dos Santos <luccas.yui@hotmail.com>
2021-01-19 15:11:34 -0300

    Adicionado rotas, componente saberMais e colecao

--- ImpactoSocial/src/app/app-routing.module.ts
+++ ImpactoSocial/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ColecaoComponent } from './colecao/colecao.component';
+import { HomeComponent } from './home/home.component';
+import { SaberMaisComponent } from './saber-mais/saber-mais.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'', redirectTo: 'home', pathMatch: 'full'},
+
+  {path:'home', component: HomeComponent},
+  {path:'saberMais', component: SaberMaisComponent},
+  {path:'colecao', component: ColecaoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ImpactoSocial/src/app/app.module.ts
+++ ImpactoSocial/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { SaberMaisComponent } from './saber-mais/saber-mais.component';
+import { ColecaoComponent } from './colecao/colecao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    SaberMaisComponent,
+    ColecaoComponent
   ],
   imports: [
     BrowserModule,
